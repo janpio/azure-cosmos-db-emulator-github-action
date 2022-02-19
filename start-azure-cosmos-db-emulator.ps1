88d8c01e7d889e705e4082144434233c45d3c423
Jan Piotrowski <piotrowski+github@gmail.com>
2022-02-19 23:32:45 +0000

    accept params

--- start-azure-cosmos-db-emulator.ps1
+++ start-azure-cosmos-db-emulator.ps1
@@ -1,3 +1,5 @@
 Write-Host "Launching Azure Cosmos DB Emulator (parameters = $args)"
 Import-Module "$env:ProgramFiles\Azure Cosmos DB Emulator\PSModules\Microsoft.Azure.CosmosDB.Emulator"
-Start-CosmosDbEmulator $args
+# https://stackoverflow.com/a/51544284
+$params = "$($args -join " ")"
+Invoke-Expression "Start-CosmosDbEmulator $params"
