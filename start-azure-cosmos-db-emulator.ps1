7dc82cb409e385b1b7df14eb8be281ba3a5bb8ad
Jan Piotrowski <piotrowski+github@gmail.com>
2022-02-19 23:50:26 +0000

    log shutdown as well and 2 test jobs

--- start-azure-cosmos-db-emulator.ps1
+++ start-azure-cosmos-db-emulator.ps1
@@ -1,9 +1,13 @@
-Write-Host "Launching Azure Cosmos DB Emulator (parameters = $args)"
 Import-Module "$env:ProgramFiles\Azure Cosmos DB Emulator\PSModules\Microsoft.Azure.CosmosDB.Emulator"
+
 # Shut down potentially already running emulator
+Write-Host "Shutting down potentially already running emulators"
 Stop-CosmosDbEmulator
+Write-Host "Done."
+
 # Start emulator with supplied parameters
 # https://stackoverflow.com/a/51544284
+Write-Host "Launching Azure Cosmos DB Emulator (parameters = $args)"
 $params = "$($args -join " ")"
 Invoke-Expression "Start-CosmosDbEmulator $params"
 Write-Host "Done."
