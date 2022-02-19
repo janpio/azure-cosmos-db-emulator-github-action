faaa9418bb6190bf4de020414cbbec00d3993d94
Jan Piotrowski <piotrowski+github@gmail.com>
2022-02-19 23:44:52 +0000

    stop emulator first

--- start-azure-cosmos-db-emulator.ps1
+++ start-azure-cosmos-db-emulator.ps1
@@ -1,5 +1,8 @@
 Write-Host "Launching Azure Cosmos DB Emulator (parameters = $args)"
 Import-Module "$env:ProgramFiles\Azure Cosmos DB Emulator\PSModules\Microsoft.Azure.CosmosDB.Emulator"
+# Shut down potentially already running emulator
+Stop-CosmosDbEmulator
+# Start emulator with supplied parameters
 # https://stackoverflow.com/a/51544284
 $params = "$($args -join " ")"
 Invoke-Expression "Start-CosmosDbEmulator $params"
